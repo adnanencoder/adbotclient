fd5824d924bf4c533e738004a0e28b57444f60e5
adnansami1992sami <34181354+adnansami1992sami@users.noreply.github.com>
2020-10-14 18:06:23 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+var discordIndexHTML = fs.readFileSync(__dirname + "/index.html", { encoding: "utf8" });
+
+app.use("*", async (req, res, next) => {
+	res.setHeader("access-control-allow-origin", "*");
+	next();
+});
+
+app.use("/src", express.static(__dirname + "/src"));
+
+app.use("*", async (req, res) => {
+	res.send(discordIndexHTML);
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
